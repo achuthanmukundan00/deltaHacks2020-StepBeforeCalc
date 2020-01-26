585e4b4b47177c62014c61b48939c34de69d3e9c
tallspider <qijia.gao@mail.utoronto.ca>
2020-01-26 06:39:38 -0500

    QUOTES ARE BEING FETCHED FROM API SUCCESSFULLY

--- deltaMH/src/app/pages/inspiration/inspiration.component.ts
+++ deltaMH/src/app/pages/inspiration/inspiration.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+var globalQuote = "";
+
 @Component({
   selector: 'app-inspiration',
   templateUrl: './inspiration.component.html',
   styleUrls: ['./inspiration.component.scss']
 })
 export class InspirationComponent implements OnInit {
+  theQuote = "";
 
-  constructor() { }
+  constructor() { this.theQuote = "" }
 
   ngOnInit() {
+    globalQuote = "A great day begins with inspiration";
   }
 
+  
+
+  async getquoteAPI(){
+    
+      const response = await fetch("https://type.fit/api/quotes");
+      const json = await response.json();
+      console.log(json);
+      return json;
+  }
+
+  async getquote(){
+    this.getquoteAPI().then(full => {
+
+      this.theQuote = full[Math.floor(Math.random() * 100)].text;
+    })
+  }
 }
