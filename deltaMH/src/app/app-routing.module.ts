0efe914c46a3f3c8b0e3b87030be4abcd6069f15
david4270 <david990724@gmail.com>
2020-01-25 21:56:39 -0500

    Made new pages. Possibly new branch?

--- deltaMH/src/app/app-routing.module.ts
+++ deltaMH/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { Page1Component } from "./pages/page1/page1.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
+  { path: "page1", component: Page1Component },
   { path: "", redirectTo: "home", pathMatch: "full" }
 ];
 
