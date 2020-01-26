4cbf45c6fa1d453e745776603885753b67f7b83a
tallspider <qijia.gao@mail.utoronto.ca>
2020-01-25 19:50:25 -0500

    first commit, skeleton pages

--- deltaMH/src/app/app-routing.module.ts
+++ deltaMH/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { JournalEditorComponent } from "./pages/journal-editor/journal-editor.component";
+import { JournalsComponent } from "./pages/journals/journals.component";
+import { CalmComponent } from "./pages/calm/calm.component";
+import { InspirationComponent } from "./pages/inspiration/inspiration.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
+  { path: "journal-editor", component: JournalEditorComponent },
+  { path: "journals", component: JournalsComponent },
+  { path: "calm", component: CalmComponent },
+  { path: "inspiration", component: InspirationComponent },
   { path: "", redirectTo: "home", pathMatch: "full" }
 ];
 
--- deltaMH/src/app/app.module.ts
+++ deltaMH/src/app/app.module.ts
@@ -5,12 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { JournalEditorComponent } from './pages/journal-editor/journal-editor.component';
+import { JournalsComponent } from './pages/journals/journals.component';
+import { CalmComponent } from './pages/calm/calm.component';
+import { InspirationComponent } from './pages/inspiration/inspiration.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    JournalEditorComponent,
+    JournalsComponent,
+    CalmComponent,
+    InspirationComponent
   ],
   imports: [
     BrowserModule,
