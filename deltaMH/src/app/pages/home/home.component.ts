141a6be1f490f835895f4aa41d1b523bd9100889
tallspider <qijia.gao@mail.utoronto.ca>
2020-01-26 05:21:19 -0500

    calm is playing music with slow color transitions suring hover

--- deltaMH/src/app/pages/home/home.component.ts
+++ deltaMH/src/app/pages/home/home.component.ts
@@ -24,22 +24,22 @@ export class HomeComponent implements OnInit {
     this.display_message = "Relax. You have it in the bag. Don't stress yourself out.";
   }
 
-  hideDiv2(message){
+  hideDiv2(){
     this.hide = true;
     this.display_message = "Almost there! Don't give up!";
   }
 
-  hideDiv3(message){
+  hideDiv3(){
     this.hide = true;
     this.display_message = "It's awesome to see you happy like this!";
   }
 
-  hideDiv4(message){
+  hideDiv4(){
     this.hide = true;
     this.display_message = "Come on! Do some jumping jacks! You'll feel the fun after.";
   }
 
-  hideDiv5(message){
+  hideDiv5(){
     this.hide = true;
     this.display_message = "Go get something delicious! You deserve it.";
   }
